99740b2138aed75ee6662bcf569e2b5943e4b15d
Muhamad Taopik <mr.94t3z@gmail.com>
2024-07-06 13:47:31 +0700

    refactor code {update cast intens also bg}

--- api/index.tsx
+++ api/index.tsx
@@ -304,7 +304,7 @@ app.frame("/tx-status", async (c) => {
           Mint +10
         </Button.Transaction>,
         <Button.Link
-          href={`https://warpcast.com/~/compose?text=degens%20love%20%2Fbasecolors%0A%0A%5BNote%3A%20a%20square%20image%20of%20your%20color%20will%20automatically%20appear%20in%20this%20cast.%20Please%20delete%20this%20note%20before%20casting%20and%20click%20the%20channel%20to%20cast%20in%20%2Fbasecolors.%5D
+          href={`https://warpcast.com/~/compose?text=arbitrum%20love%20%2Fbasecolors%0A%0A%5BNote%3A%20a%20square%20image%20of%20your%20color%20will%20automatically%20appear%20in%20this%20cast.%20Please%20delete%20this%20note%20before%20casting%20and%20click%20the%20channel%20to%20cast%20in%20%2Fbasecolors.%5D
   &embeds[]=https://hexcolorserver.replit.app/${hex.substring(
             1
           )}.png&embeds[]=https://warpcast.com/jake/0xb4da666b`}
@@ -396,7 +396,7 @@ app.frame("/leaderBoard/:address", async (c) => {
         Mint +10
       </Button.Transaction>,
       <Button.Link 
-        href={`https://warpcast.com/~/compose?text=degens%20love%20%2Fbasecolors%0A%0A[Note:%20a%20square%20image%20of%20the%20Base%20Colors%20logo%20will%20automatically%20appear%20in%20this%20cast.%20Please%20delete%20this%20note%20before%20casting%20and%20click%20the%20channel%20to%20cast%20in%20%2Fbasecolors.]&embeds[]=https://i.ibb.co/PtwcHP7/base-spectrum-square.jpg&embeds[]=https://warpcast.com/jake/0xb4da666b`}
+        href={`https://warpcast.com/~/compose?text=arbitrum%20love%20%2Fbasecolors%0A%0A[Note:%20a%20square%20image%20of%20the%20Base%20Colors%20logo%20will%20automatically%20appear%20in%20this%20cast.%20Please%20delete%20this%20note%20before%20casting%20and%20click%20the%20channel%20to%20cast%20in%20%2Fbasecolors.]&embeds[]=https://i.ibb.co/PtwcHP7/base-spectrum-square.jpg&embeds[]=https://warpcast.com/jake/0xb4da666b`}
       >
         Share
       </Button.Link>,
@@ -443,7 +443,7 @@ app.image("/leaderBoardImage/:address", async (c) => {
           flexDirection: "column",
           fontSize: 20,
           width: "600px",
-          backgroundColor: "#ffffff",
+          backgroundColor: "#D4E4FE",
           height: "600px",
           alignItems: "center",
           justifyContent: "center",
